75d859a3ad4f50bc98f9193e5dd8387306c00f9a
Gustavo6969 <gustavohdzp1821@gmail.com>
2021-10-28 20:19:31 -0500

    listo para producción

---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProductoInterface } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  productos: ProductoInterface[] = []; 
+  productosFiltrado: ProductoInterface[] = [];
+
+
+  constructor( private http: HttpClient ) {
+
+    this.cargarProductos();
+
+   }
+    
+
+    private cargarProductos() {
+
+      return new Promise( ( resolve, reject ) => {
+
+        this.http.get('https://angular-html-b94df-default-rtdb.firebaseio.com/productos_idx.json')
+            .subscribe( (resp: ProductoInterface[]) => {
+              this.productos = resp;
+              this.cargando = false;
+            });
+
+      });
+
+    }
+
+    getProducto(id: string ) {
+
+      return this.http.get(`https://angular-html-b94df-default-rtdb.firebaseio.com/productos/${ id }.json`)
+
+    }
+
+    buscarProducto(termino: string) {
+
+      if ( this.productos.length === 0 ){
+        // cargar productos
+        this.cargarProductos().then( ()=> {
+          // ejecutar despues de tener los productos
+          // aplicar filtro  
+          this.filtrarProductos( termino );
+        });
+
+      } else {
+        // aplicar el filtro
+        this.filtrarProductos( termino );
+      }
+
+
+    }
+
+    private filtrarProductos( termino: string ) {
+
+      // console.log(this.productos);
+      this.productosFiltrado = [];
+
+      termino = termino.toLocaleLowerCase( );
+
+      this.productos.forEach( prod => {
+
+        const tituloLower = prod.titulo.toLocaleLowerCase();
+
+        if ( prod.categoria.indexOf( termino ) >= 0 || tituloLower.indexOf( termino ) >= 0 ) {
+          this.productosFiltrado.push( prod );
+
+        }
+
+      })
+
+    }
+
+}
